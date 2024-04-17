737e891d2bbfc240b1be5a97e0fadde3c9a591f8
k_furuta <furuta.kyohei.789@gmail.com>
2024-04-17 23:41:25 +0900

    コメントを足した

--- ex1/k_furuta/main.py
+++ ex1/k_furuta/main.py
@@ -3,19 +3,22 @@ import matplotlib.pyplot as plt
 import librosa
 import sys
 
+#音声波形からスペクトログラムを作成
 def create_spectrogram(signal, n_fft, hop_length):
-    n_frames = 1 + int((len(signal) - n_fft) / hop_length)
-    spectrogram = np.zeros((n_fft, n_frames), dtype=np.complex_)
+    n_frames = 1 + int((len(signal) - n_fft) / hop_length) #窓のサイズと窓ごとの間隔でフレーム数を決定
+    spectrogram = np.zeros((n_fft, n_frames), dtype=np.complex_) #データを格納するndarrayを作成
 
+    #フレームごとに窓関数を掛けてスペクトルを計算
     for i in range(n_frames):
         frame = signal[i * hop_length:i * hop_length + n_fft]
-        frame = np.pad(frame, (0, max(0, n_fft - len(frame))), mode='constant')
+        frame = np.pad(frame, (0, max(0, n_fft - len(frame))), mode='constant') #フレームが足りない場合は0埋め
         windowed = frame * np.hanning(n_fft)
         fft_result = np.fft.fft(windowed)
         spectrogram[:, i] = fft_result
 
     return spectrogram
 
+#スペクトログラムから音声波形を復元
 def inverse_spectrogram(spectrogram, n_fft, hop_length):
     n_frames = spectrogram.shape[1]
     output_length = n_fft + hop_length * (n_frames - 1)
@@ -24,21 +27,22 @@ def inverse_spectrogram(spectrogram, n_fft, hop_length):
     for i in range(n_frames):
         frame = np.fft.ifft(spectrogram[:, i])
         start = i * hop_length
-        signal[start:start + n_fft] += frame.real * np.hanning(n_fft)  # ifftの実数部分のみ利用
-
+        # ifftの実数部分のみ利用(丸め誤差で微小な虚部が発生しているため、結果には影響しない)
+        signal[start:start + n_fft] += frame.real * np.hanning(n_fft)  
     return signal
 
-# ファイルを読み込む
+# コマンドライン引数ファイルを読み込む
 args = sys.argv
 if len(args)==1:
+    #ファイル名未指定の場合のエラー処理
     print("usage:main.py INPUT_FILE")
     exit()
+
 filename = args[1]
-signal, sr = librosa.load(filename, sr=None)
-time = np.arange(0, len(signal)) / sr
+signal, rate = librosa.load(filename, sr=None)
 
 # パラメータ
-n_fft = 2048
+n_fft = 1024
 hop_length = 512
 
 # スペクトログラムの生成
@@ -47,7 +51,9 @@ spectrogram = create_spectrogram(signal, n_fft, hop_length)
 # 逆変換
 reconstructed_signal = inverse_spectrogram(spectrogram, n_fft, hop_length)
 
-# スペクトログラムの表示
+# スペクトログラムと信号の表示
+time = np.arange(0, len(signal)) / rate # 音声波形をプロットするための時間データ
+
 plt.subplots_adjust(wspace=0.4, hspace=1.0)
 plt.subplot(221)
 plt.plot(time, signal)
@@ -56,11 +62,17 @@ plt.xlabel("Time [sec]")
 plt.ylabel("Amplitude")
 
 plt.subplot(122)
-plt.imshow(20 * np.log10(np.abs(spectrogram)[:n_fft // 2, :]), aspect='auto', origin='lower', cmap='viridis')
-plt.colorbar(format='%+2.0f dB')
+#x軸とy軸の表示範囲
+#  x軸は音声の時間
+#  サンプリング定理よりy軸はサンプリング周波数の半分？
+extent = (0, len(signal)/rate, 0, rate/2)
+#実数信号なので周波数はsymmetricになっているため半分だけ表示
+#デシベルと振幅の変換 振幅:x -> デシベル:20*log_10(x) 
+plt.imshow(20 * np.log10(np.abs(spectrogram)[:n_fft // 2, :]), extent=extent, aspect='auto', origin='lower', cmap='viridis')
+
 plt.title('Spectrogram')
-plt.xlabel('Time Frame')
-plt.ylabel('Frequency Bin')
+plt.xlabel('Time [sec]')
+plt.ylabel('Frequency [hz]')
 
 plt.subplot(223)
 plt.plot(time, reconstructed_signal)
