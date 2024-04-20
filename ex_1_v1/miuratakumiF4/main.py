d2aea9dd49383c6c0db6a6f5ac87ebe2afc9968a
miuratakumiF4 <miura.takumi@g.sp.m.is.nagoya-u.ac.jp>
2024-04-20 18:10:31 +0900

    add get_spectrogram and restore_waveform func and so on

--- ex_1_v1/miuratakumiF4/main.py
+++ ex_1_v1/miuratakumiF4/main.py
@@ -1,98 +1,146 @@
-"""This module generates a spectrogram and performs an inverse transform.
-
-It reads an input WAV file and generates a spectrogram.
-The script then performs
-an inverse transform to obtain the original signal.
-
-"""
-import argparse
-
-import matplotlib.pyplot as plt
+import sys
+import wave
 import numpy as np
-import scipy.fftpack as fftpack
-import scipy.io.wavfile as wavfile
-import scipy.signal as signal
+import matplotlib.pyplot as plt
 
+from scipy import signal
 
-def parse_args():
-    """Retrieve variables from the command prompt."""
-    parser = argparse.ArgumentParser(
-        description="Generate spectrogram and inverse transform"
-    )
-    parser.add_argument("--input-file", type=str, required=True, help="input wav file")
-    """add nfft."""
-    parser.add_argument("--nfft", type=int, default=1024, help="number of FFT points")
-    parser.add_argument(
-        "--hop-length",
-        type=int,
-        default=512,
-        help="number of samples between successive STFT columns",
-    )
-    parser.add_argument(
-        "--window", type=str, default="hann", help="window function type"
-    )
-    return parser.parse_args()
+def get_filename():
+    # コマンドライン引数を取得
+    args=sys.argv   # args=["main.py",str(FILENAME)]
 
+    if len(args)==2:
+        return args[1]
+    
+    else:
+        # コマンドライン引数に過不足がある場合,USAGEを表示
+        print("USAGE: main.py FILENAME")
+        return
+    
+def get_wavedata(filename:str):    
+    # Wave_read objectを取得
+    wave_read_obj=wave.open(filename, mode='rb')
 
-def main():
-    """Generate spectrograms and inverse transforms of audio signals."""
-    args = parse_args()
+    # バイナリデータを取得
+    wavedata_bin=wave_read_obj.readframes(-1)
 
-    # 音声ファイルを読み込む
-    rate, data = wavfile.read(args.input_file)
-    data = np.array(data, dtype=float)
+    # バイナリデータをndarrayに変換
+    if wave_read_obj.getsampwidth()==2:
+        wavedata_ndarray=np.frombuffer(wavedata_bin,dtype='int16')
+        
+    elif wave_read_obj.getsampwidth()==4:
+        wavedata_ndarray=np.frombuffer(wavedata_bin,dtype='int32')
 
-    # 波形をプロットする
-    time = np.arange(0, len(data)) / rate
-    plt.plot(time, data)
+    else:
+        print("Wave read object sample width error")
+        return
+
+    # サンプル周波数を取得
+    framerate=wave_read_obj.getframerate()  # [Hz]
+    sampling_rate=1/framerate   # [s]
+    
+    return wavedata_ndarray,sampling_rate
+    
+def plot_waveform(wavedata_ndarray:np.ndarray,sampling_rate:float,picture_name_option:str=""):
+    # 波形を表示する準備
+    x_val=np.arange(len(wavedata_ndarray))*sampling_rate    # 横軸の定義
+    plt.plot(x_val,wavedata_ndarray)
+    plt.title("Waveform{0}".format(picture_name_option))
     plt.xlabel("Time [sec]")
     plt.ylabel("Amplitude")
+
+    # 画像として保存
+    plt.savefig("waveform{0}.png".format(picture_name_option))
+    
+    # 波形を表示
     plt.show()
 
-    # STFTのそれぞれのパラメータ
-    nfft = args.nfft
-    hop_length = args.hop_length
-    window = args.window
-    window_func = signal.get_window(window, nfft)
+def get_spectrogram(wavedata_ndarray:np.ndarray,n_samples:int,skip_width:int):
+    # 空の行列を準備
+    spectrogram=np.zeros((1+(len(wavedata_ndarray)-n_samples)//skip_width,n_samples//2),dtype=np.complex128)
 
-    # スペクトログラムの計算
-    spectrogram = np.zeros(
-        (1 + nfft // 2, (len(data) - nfft) // hop_length + 1), dtype=np.complex128
-    )
-    for i in range(spectrogram.shape[1]):
-        segment = data[i * hop_length : i * hop_length + nfft] * window_func
-        spectrum = fftpack.fft(segment, n=nfft, axis=0)[: 1 + nfft // 2]
-        spectrogram[:, i] = spectrum
+    # 窓関数
+    window=signal.hann(n_samples)
 
-    # スペクトログラムの描画
-    plt.figure()
-    plt.imshow(
-        20 * np.log10(np.abs(spectrogram)), origin="lower", aspect="auto", cmap="jet"
-    )
-    plt.xlabel("Time (s)")
-    plt.ylabel("Frequency (Hz)")
-    plt.colorbar()
+    for i in range(1+(len(wavedata_ndarray)-n_samples)//skip_width):
+        # fftを行う領域を切り出し、窓関数をかける
+        sample=wavedata_ndarray[i*skip_width:i*skip_width+n_samples]*window
+
+        # スペクトルを計算
+        fft_sample=np.fft.fft(sample,n=n_samples,axis=0)[:n_samples//2]   # fft
+        spectrogram[i]=fft_sample  # 記録
+
+    return spectrogram
+
+
+def plot_spectrogram(spectrogram:np.ndarray,sampling_rate:float,n_samples:int,skip_width:int):
+    # スペクトログラムを表示する準備
+    amp=np.abs(spectrogram)    # 振幅を計算
+    amp_nonzero=np.where(amp==0,np.nan,amp)   # divide by zero encountered in log10を回避するため0をnanに置き換え
+    x_val=np.arange(len(spectrogram))*sampling_rate*skip_width    # 横軸の定義
+    y_val=np.fft.fftfreq(n_samples,d=sampling_rate)[:n_samples//2]    # 縦軸の定義
+    plt.pcolor(x_val,y_val,20*np.log10(amp_nonzero).T,shading="auto",cmap="jet")
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Frequency [Hz]")
     plt.title("Spectrogram")
+    plt.colorbar()
+
+    # 画像として保存
+    plt.savefig("spectrogram.png")
+
+    # スペクトログラムを表示
     plt.show()
 
-    # 逆変換の計算
-    time_signal = np.zeros(len(data))
-    for i in range(spectrogram.shape[1]):
-        spectrum = spectrogram[:, i]
-        segment = fftpack.ifft(spectrum, n=nfft, axis=0)
-        segment = np.real(segment) * window_func
-        time_signal[i * hop_length : i * hop_length + nfft] += segment
+def restore_waveform(spectrogram:np.ndarray,n_samples:int,skip_width:int):
+    # 空の配列を準備
+    restore_waveform_ndarray=np.zeros((len(spectrogram)-1)*skip_width+n_samples)
+    
+    # 窓関数
+    window=signal.hann(n_samples)
 
-    # 逆変換した波形のプロット
-    plt.figure()
-    plt.plot(time, time_signal)
-    plt.xlabel("Time [sec]")
-    plt.ylabel("Amplitude")
-    plt.title("Inverse Transform")
+    for i in range(len(spectrogram)):
+        # フーリエ逆変換
+        spectol=spectrogram[i]  # スペクトル切り出し
+        wave_piece=np.fft.ifft(spectol,n=n_samples,axis=0).real # ifft
+        wave_piece=wave_piece*window    # 窓関数をかける
+        restore_waveform_ndarray[i*skip_width:i*skip_width+n_samples]+=wave_piece
 
-    plt.show()
+    return restore_waveform_ndarray
+    
+def main():
+    # 音声ファイル名を取得
+    filename=get_filename()
 
-#gittestcomment
+    # 音声ファイル名を取得できなければ終了
+    if filename is None:
+        return
+    
+    # 波形のndarrayを取得
+    wavedata_ndarray,sampling_rate=get_wavedata(filename)
 
-if __name__ == "__main__":
+    # 波形のndarrayを取得できなければ終了
+    if wavedata_ndarray is None:
+        return
+    
+    # 波形を出力
+    plot_waveform(wavedata_ndarray,sampling_rate,picture_name_option="_original")
+
+    # パラメータ
+    N_SAMPLES=1024
+    SKIP_WIDTH=N_SAMPLES//2
+    
+    # スペクトログラムの計算
+    spectrogram=get_spectrogram(wavedata_ndarray,N_SAMPLES,SKIP_WIDTH)
+
+    # スペクトログラムの表示
+    plot_spectrogram(spectrogram,sampling_rate,N_SAMPLES,SKIP_WIDTH)
+
+    # フーリエ逆変換で波形を再計算
+    restore_wavedata_ndarray=restore_waveform(spectrogram,N_SAMPLES,SKIP_WIDTH)
+
+    # 再計算した波形を出力
+    plot_waveform(restore_wavedata_ndarray,sampling_rate,picture_name_option="_restored")
+
+
+if __name__=='__main__':
     main()
