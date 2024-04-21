d107f71fb3b840cc056829ac79361afa26f5c5d1
kyo73 <namizaki.kyousuke@g.sp.m.is.nagoya-u.ac.jp>
2024-04-21 22:23:28 +0900

    end?

--- k_namizaki/main1.py
+++ k_namizaki/main1.py
@@ -1,68 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io.wavfile as wavfile
+import soundfile as sf
 import scipy.signal as signal
 import scipy.fftpack as fftpack
+import argparse
+
 
 def main():
+    parser = argparse.ArgumentParser(description="wavfileからスペクトログラムを描画し、元に戻す")
+    parser.add_argument("-file", help="ファイルを入力")
+    args = parser.parse_args() 
     # データを読み込み
-    rate, data = wavfile.read(r"C:\Users\kyskn\B4Lecture-2024\k_namizaki\reco2mono.wav")
+    data, rate = sf.read(args.file)
 
     # 波形データをplot
-    time = np.arange(0, len(data)) / rate # 0~最後までを周波数で割る
+    time = np.arange(0, len(data)) / rate
     plt.plot(time, data)
-    plt.xlabel("Time [sec]")
+    plt.xlabel("Time [s]")
     plt.ylabel("Amplitude[unknown]")
     plt.title("Before")
     plt.show()
 
     # FFTのパラメータ
-    nfft = 1024  # FFTサイズ
-    hop_length = 512  # フレーム間のシフト量
+    N = 1024 # FFTサイズ
+    shift = N // 2 # フレーム間のシフト量
     
-    window =  signal.get_window("hann", nfft)
+    window =  signal.get_window("hann", N) # 窓関数
 
     # スペクトログラムの計算
-    # スペクトログラムの初期化 np.zeros(行,列)
-    spectrogram = np.zeros(
-        (1 + nfft // 2, (len(data) - nfft) // hop_length + 1), dtype=np.complex128
-    )
-    # spectrogramの列数だけ回す
+    # スペクトログラムを格納(dtype=complexじゃないと複素数を実数に格納しようとしていることになってしまう)
+    spectrogram = np.zeros([N // 2 , len(data) // shift - (N // shift - 1)], dtype=complex)
+
     for i in range(spectrogram.shape[1]):
-        # 分割したものに窓関数をかける
-        segment = data[i * hop_length : i * hop_length + nfft] * window
-        # fftする[0 : 1 + nfft // 2]分だけ取り出す
-        spectrum = fftpack.fft(segment, n=nfft)[: 1 + nfft // 2]
-        # spectrogrumのi列目に保管
-        spectrogram[:, i] = spectrum
+        # 窓関数をかける
+        segment = data[i * shift : i * shift + N] * window
+        # fft
+        spectrum = fftpack.fft(segment, n=N)[: N // 2]
+        # スペクトログラムに格納
+        spectrogram[:,i] = spectrum
         
-    # スペクトログラムの描画
+    # デシベル変換
+    dB_spectrogram = np.zeros([N // 2 , len(data) // shift - (N // shift - 1)])
+    eps = np.finfo(float).eps # ゼロ除算を回避するための微小な値
+    dB_spectrogram = 20 * np.log10(np.abs(spectrogram) + eps)
     
+    # スペクトログラムの表示
     plt.figure()
-    # plt.imshow()
-    plt.imshow(
-        20 * np.log10(np.abs(spectrogram)), origin="lower", aspect="auto", cmap="jet"
+    # imshowは2次元配列をカラーマップで表示(originは原点の位置)
+    plt.imshow(dB_spectrogram,
+               origin="lower",
     )
     plt.xlabel("Time [s]")
     plt.ylabel("Frequency [Hz]")
-    plt.colorbar()
     plt.title("Spectrogram")
+    plt.colorbar()
     plt.show()
-
+    
     # 逆フーリエ変換を使ってオリジナルの波形データを再構築
-    after_data = np.zeros(len(data), dtype=np.float64)
-    # 0から len(data)-nfft-1 までの整数を、hop_length分シフトしながら区切る
-    for i in range(0, len(data)-nfft, hop_length):
-        # spectrogramの i//hop_length列目を保管
-        spectrum = spectrogram[:, i//hop_length]
-        # 逆フーリエ
-        frame = np.fft.irfft(spectrum, n=nfft)
-        # i:i+nfft の範囲に、波形データframeを加算
-        after_data[i:i+nfft] += frame
-
+    after_data = np.zeros(len(data))
+    for i in range(spectrogram.shape[1]):
+        spectrum = spectrogram[:, i]
+        segment = fftpack.ifft(spectrum, n=N)
+        segment = np.real(segment) * window
+        after_data[i * shift : i * shift + N] += segment
+    
     # オリジナルの波形データをplot
     plt.figure()
-    plt.plot(time, after_data[:len(time)])
+    plt.plot(time, after_data)
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude[unknown]")
     plt.title("After")
