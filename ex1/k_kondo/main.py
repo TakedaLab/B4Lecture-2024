0fc6946112375a485eafda21fff714084fbd431a
Ken78K <kondo.0626.kenta@gmail.com>
2024-04-23 22:44:19 +0900

    linter通したい

--- ex1/k_kondo/main.py
+++ ex1/k_kondo/main.py
@@ -9,15 +9,18 @@ def STFT(data, Lw, step):
     # data 信号,　Lw 窓幅, step 切り出し幅
     l = data.shape[0]
     win = np.hanning(Lw)
-    Mf = Lw//2 + 1
-    Nf = int(np.ceil((l-Lw+step)/step))
+    Mf = Lw // 2 + 1
+    Nf = int(np.ceil((l - Lw + step) / step))
     S = np.empty([Mf, Nf], dtype=np.complex128)
     for i in range(Nf):
         start = int(i * step)
         end = int(start + Lw)
-        segment = data[start:end] if end <= l else np.append(data[start:], np.zeros(end - l))
+        segment = (
+            data[start:end] if end <= l else np.append(data[start:], np.zeros(end - l))
+        )
         S[:, i] = np.fft.rfft(segment * win, n=Lw, axis=0)
     return S
+
 
 def ISTFT(S, Lw, step):
     # 逆変換の実行
@@ -35,31 +38,32 @@ def ISTFT(S, Lw, step):
 
     return x
 
+
 def main():
     # コマンドプロンプトからファイル名を受け取り
     args = sys.argv
-    if(len(args) == 2):
+    if len(args) == 2:
         file_name = args[1]
     else:
         print("Usage: python main.py <file_name>")
 
     rate, data = wavfile.read(file_name)
     data = np.array(data, dtype=float)
-    time = np.arange(0, len(data))/rate
+    time = np.arange(0, len(data)) / rate
 
     # 波形を表示
     plt.plot(time, data)
     plt.xlabel("Time[sec]")
     plt.ylabel("Amplitude")
     plt.title("original_signal")
-    
+
     length_window = 512
-    step = length_window/4
+    step = length_window / 4
 
     spectrogrum = STFT(data, length_window, step)
-    
+
     # スペクトログラムを表示
-    P = 20*np.log10(np.abs(spectrogrum))
+    P = 20 * np.log10(np.abs(spectrogrum))
     plt.figure()
     plt.imshow(P, origin="lower", aspect="auto")
     plt.xlabel("Time[sec]")
@@ -77,6 +81,7 @@ def main():
     plt.ylabel("Amplitude")
     plt.title("inverse transform")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
