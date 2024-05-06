20390aa11c3ff589336483e9ed376f054888aab3
kyo73 <namizaki.kyousuke@g.sp.m.is.nagoya-u.ac.jp>
2024-05-06 10:09:42 +0900

    check

--- ex3/k_namizaki/main3.py
+++ ex3/k_namizaki/main3.py
@@ -1,33 +1,36 @@
 """
 This code is to do regression analyzed using the least squares method.
+
+Calculate weights.
+Plot data.
 """
+
 import argparse
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def parse_args():
     """
-    Get Arguments
+    Get Arguments.
 
     Returns
     -------
     parser.parse_args() : 引数を返す
     """
-    parser = argparse.ArgumentParser(
-        description="最小二乗法を用いて回帰分析を行う。"
-    )
+    parser = argparse.ArgumentParser(description="最小二乗法を用いて回帰分析を行う。")
     parser.add_argument(
         "-file",
         help="ファイルを入力",
         default=r"C:\Users\kyskn\B4Lecture-2024\ex3\k_namizaki\data2.csv",
-        type = str
+        type=str,
     )
     parser.add_argument("-n", help="次数", default=1, type=int)
     parser.add_argument("-normal", help="正則化係数", default=0, type=int)
     return parser.parse_args()
 
-def plot2d(x,y,w):
+def plot2d(x, y, w):
     """
     Plot in 2 dimensions.
 
@@ -47,10 +50,11 @@ def plot2d(x,y,w):
     fig, ax = plt.subplots()
     ax.set_xlabel("X axis")
     ax.set_ylabel("Y axis")
-    ax.scatter(x, y, label = "Observed data")
+    ax.scatter(x, y, label ="Observed data")
     ax.plot(x_ans, y_ans, label="ans", color="r")
     ax.legend()
     plt.show()
+
 
 def plot3d(x, y, z, w, degree):
     """
@@ -80,16 +84,17 @@ def plot3d(x, y, z, w, degree):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
     ax.scatter(x, y, z, label="Observed data")
-    ax.plot_surface(X, Y, z_ans, label="ans",color="r", alpha=0.3)
+    ax.plot_surface(X, Y, z_ans, label="ans", color="r", alpha=0.3)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
     ax.legend()
     plt.show()
 
+
 def weight2d(x, y, degree, normal):
     """
-    Calculate weights from 2 sets of data
+    Calculate weights from 2 sets of data.
 
     Parameters
     ----------
@@ -116,9 +121,10 @@ def weight2d(x, y, degree, normal):
     w = np.linalg.inv(X.T @ X + normal * I) @ X.T @ Y
     return w
 
+
 def weight3d(x, y, z, degree, normal):
     """
-    Calculate weights from 3 sets of data
+    Calculate weights from 3 sets of data.
 
     Parameters
     ----------
@@ -133,7 +139,7 @@ def weight3d(x, y, z, degree, normal):
     w : 重み
     """
     # X = 1+x+x^2+...+y+y^2+...
-    X = np.zeros((len(x), 2*degree + 1))
+    X = np.zeros((len(x), 2 * degree + 1))
     Z = z
     # 0~次数まで回す
     for i in range(degree + 1):
@@ -148,35 +154,29 @@ def weight3d(x, y, z, degree, normal):
             j += 1
         i += 1
     # w = (X.T @ X + λ @ I)^-1 @ X.T @ Y
-    w = np.zeros(2*degree + 1)
+    w = np.zeros(2 * degree + 1)
     I = np.identity(len(X[0]))
     w = np.linalg.inv(X.T @ X + normal * I) @ X.T @ Z
     return w
 
+
 def main():
-    """
-    Do the regression analyzed using the least squares method.
-    """
+    """Do the regression analyzed using the least squares method."""
     args = parse_args()
-    data = np.loadtxt(
-        args.file,
-        comments="x1",
-        delimiter=",",
-        dtype="float"
-    )
+    data = np.loadtxt(args.file, comments="x1", delimiter=",", dtype="float")
     degree = args.n
     normal = args.normal
 
     if len(data[0]) == 2:
-        #データ収納
+        # データ収納
         x = data[:, 0]
         y = data[:, 1]
         # 重みwを計算
         w = weight2d(x, y, degree, normal)
         print(w)
-        plot2d(x,y,w)
+        plot2d(x, y, w)
     elif len(data[0]) == 3:
-        #データ収納
+        # データ収納
         x = data[:, 0]
         y = data[:, 1]
         z = data[:, 2]
