6f265efbc9bfe94aa3fb034e8fd1882f714aec32
k_furuta <furuta.kyohei.789@gmail.com>
2024-05-12 13:03:44 +0900

    プログラム名の変更と不要なファイルの削除

--- ex4/k_furuta/rotate3D.py
+++ ex4/k_furuta/rotate3D.py
@@ -11,14 +11,15 @@ ax = fig.add_subplot(111, projection='3d')
 
 # csvファイルの読み込み
 data = np.genfromtxt("./data2.csv", delimiter=",", skip_header=1)
+# データを標準化
 data = scipy.stats.zscore(data)
 
+# 主成分を求める
 w,v,cr = main.calc_PCA(data)
-print(v)
-
+# プロット用に軸ごとに分割
 X,Y,Z = data[:,0],data[:,1],data[:,2]
 
-
+# ここを書き換えて描画対象を変更
 def init():
     ax.scatter(X,Y,Z)
     main.axline3D(ax, 0, v[:,0], scale=3, color='red', linewidth=2)
@@ -26,11 +27,12 @@ def init():
     main.axline3D(ax, 0, v[:,2], scale=3, color='blue', linewidth=2)
     return fig,
 
+# ここは書き換えない
 def animate(i):
     ax.view_init(elev=30., azim=3.6*i)
     return fig,
 
-# Animate
+# Animationを作成
 ani = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=100, interval=100, blit=True)    
 ani.save('rotate.gif', writer="ffmpeg",dpi=100)
