ae78bfa25bf4e9e55658557249567b2aeca2ccd9
MiyajiHikari <wr.57r.0534@s.thers.ac.jp>
2024-05-25 14:35:16 +0900

    add docstrings

--- ex6/h_miyaji/main.py
+++ ex6/h_miyaji/main.py
@@ -1,4 +1,4 @@
-"""This module performs a fitting of the data using the EM algorithm."""
+"""This module makes predictions of HMM."""
 
 import argparse
 import os
@@ -12,19 +12,29 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 class HMM:
+    """Define several HMMs and make predictions for the model.
+
+    The Forward and Viterbi algorithm can be performed.
+    It can also display the prediction results in a confusion matrix.
+    """
 
     def __init__(self, file_name: str) -> None:
+        """Initialize HMMs.
+
+        Args:
+            file_name (str): pickle file name.
+        """
         # load pickle data
         data = pickle.load(open(file_name, "rb"))
 
-        # set HMM
+        # set HMMs
         self.answer_models: np.ndarray = np.array(data["answer_models"])  # (p, )
         self.output: np.ndarray = np.array(data["output"])  # (p, t)
         self.PI: np.ndarray = np.array(data["models"]["PI"])  # (k, l, 1)
         self.A: np.ndarray = np.array(data["models"]["A"])  # (k, l, l)
         self.B: np.ndarray = np.array(data["models"]["B"])  # (k, l, n)
 
-        # set value
+        # set constant
         self.OUT_NUM: int = self.output.shape[0]  # p
         self.OUT_LEN: int = self.output.shape[1]  # t
         self.MODEL_NUM: int = self.B.shape[0]  # k
@@ -32,6 +42,11 @@ class HMM:
         self.SYMBOL_NUM: int = self.B.shape[2]  # n
 
     def forward_algorithm(self) -> np.ndarray:
+        """Make predictions of HMM with the Forward algorithm.
+
+        Returns:
+            np.ndarray: the results of HMM predictions (p, ).
+        """
         alpha = np.zeros((self.MODEL_NUM, self.STATE_NUM, self.OUT_LEN))  # (k, l, t)
         probability = np.zeros((self.OUT_NUM, self.MODEL_NUM))  # (p, k)
         predicted_model = np.zeros(self.OUT_NUM, dtype=int)  # (p, )
@@ -53,6 +68,11 @@ class HMM:
         return predicted_model
 
     def viterbi_algorithm(self) -> np.ndarray:
+        """Make predictions of HMM with the Viterbi algorithm.
+
+        Returns:
+            np.ndarray: the results of HMM predictions (p, ).
+        """
         delta = np.zeros((self.MODEL_NUM, self.STATE_NUM, self.OUT_LEN))  # (k, l, t)
         probability = np.zeros((self.OUT_NUM, self.MODEL_NUM))  # (p, k)
         predicted_model = np.zeros(self.OUT_NUM, dtype=int)  # (p, )
@@ -75,8 +95,18 @@ class HMM:
 
     def display_result(
         self, predicted_model: np.ndarray, run_time: float, algorithm_name: str, ax
-    ):
+    ) -> None:
+        """Display the prediction results in a confusion matrix.
 
+        Args:
+            predicted_model (np.ndarray): the results of HMM predictions (p, ).
+            run_time (float): algorithm run time.
+            algorithm_name (str): the name of algorithm used for predictions.
+            ax (Axes): Axes object indicating where to display the figure.
+
+        Returns:
+            None
+        """
         cm = confusion_matrix(self.answer_models, predicted_model)
         accuracy = accuracy_score(self.answer_models, predicted_model) * 100
         sns.heatmap(cm, ax=ax, annot=True, cmap="gray_r", cbar=False, square=True)
@@ -90,7 +120,7 @@ class HMM:
 
 def parse_args() -> argparse.Namespace:
     """Retrieve variables from the command prompt."""
-    parser = argparse.ArgumentParser(description="performs a fitting of tha data.")
+    parser = argparse.ArgumentParser(description="makes predictions of HMM.")
 
     # pickle file name
     parser.add_argument(
@@ -101,10 +131,10 @@ def parse_args() -> argparse.Namespace:
 
 
 def main():
-    """Load csv file and perform a fitting of the data using the EM algorithm."""
+    """Load pickle file and makes predictions of HMM."""
     # get argument
     args = parse_args()
-    file_name = args.input_file  # csv pickle name
+    file_name = args.input_file  # pickle file name
 
     # make an instance of HMM
     hmm = HMM(file_name)
