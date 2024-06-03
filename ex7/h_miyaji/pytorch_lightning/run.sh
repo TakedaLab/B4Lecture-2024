d904f5c2514c3f699977642c992b9c42ced73e11
MiyajiHikari <wr.57r.0534@s.thers.ac.jp>
2024-06-03 22:03:02 +0900

    commit test_figs, run.sh

--- ex7/h_miyaji/pytorch_lightning/run.sh
+++ ex7/h_miyaji/pytorch_lightning/run.sh
@@ -2,4 +2,5 @@
 
 source .venv3810_2/bin/activate
 
-python ./h_miyaji/pytorch_lightning/main.py
+# python ./h_miyaji/pytorch_lightning/main.py
+python ./h_miyaji/pytorch_lightning/main.py --path_to_truth ./test_truth.csv
