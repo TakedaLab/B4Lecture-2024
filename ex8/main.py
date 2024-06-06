dedb27a617fe5f28265236384702c550f52a328c
Rikuhei-ynwa <umemoto.rikuhei@g.sp.m.is.nagoya-u.ac.jp>
2024-06-06 13:40:28 +0900

    fix format

--- ex8/main.py
+++ ex8/main.py
@@ -9,8 +9,8 @@ import torch
 from torch import optim
 from torchvision import datasets, transforms
 
-from libs.Visualize import Visualize
 from models.VAE import VAE
+from libs.Visualize import Visualize
 
 
 class Main:
